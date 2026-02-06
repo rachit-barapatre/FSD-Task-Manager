b02a7731c9180396027d92d8b287f910645f5541
RachitBarapatre <rachit8104barapatre@gmail.com>
2026-02-06 10:12:21 +0530

    Allow config file for Vercel

---
+++ js/config.js
@@ -0,0 +1,13 @@
+// js/config.js
+
+// Yahan apni asli keys daal de
+export const API_KEY = "AIzaSyD5h2WpUtA6WVG5-xXA3FpFkvsW4dtrvkk";
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBhmspI5mFEoTw7S1VuLfp_8S-QqBullXw", 
+    authDomain: "pro-task-board.firebaseapp.com",
+    projectId: "pro-task-board",
+    storageBucket: "pro-task-board.firebasestorage.app",
+    messagingSenderId: "527388918652",
+    appId: "1:527388918652:web:7ee8c1f273837d2e1c5af9"
+};
